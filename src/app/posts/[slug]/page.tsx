78f0285affd3c00c097a8e16cdcabf49322bea7f
seungmileee <dltmdal0928@naver.com>
2023-05-11 13:17:25 +0900

    feat: post 페이지 스타일링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -14,7 +14,7 @@ export default async function PostPage({ params: { slug } }: Props) {
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
       <Image
-        className="w-full h-1/5"
+        className="w-full h-1/5 max-h-[500px]"
         src={`/image/posts/${path}.png`}
         alt={title}
         width={760}
