939e576524f7de826b6152bf8e9c1a9189665773
seungmileee <dltmdal0928@naver.com>
2023-05-11 12:15:14 +0900

    feat: Post 페이지 완성 (PostGrid, PostCard 이용해 Posts들 정렬), FilteredPost, Categories 컴포넌트 만들어 카테고리별로 Post 필터링 하는 기능 완성

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,15 @@
-export default function PostsPage() {
-  return <p>포스트들</p>;
+import FilterablePost from "@/components/FilterablePost";
+import { getAllPosts } from "@/service/post";
+
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  // 중복제거 후, 고유한 카테고리 아이템만 담은 배열 완성
+  const categories = [...new Set(posts.map((post) => post.category))];
+
+  return (
+    <section className="my-6">
+      <h2 className="text-2xl font-bold my-2">Posts</h2>
+      <FilterablePost posts={posts} categories={categories} />
+    </section>
+  );
 }
---
+++ src/components/FilterablePost.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Post } from "@/service/post";
+import { useState } from "react";
+import Categories from "./Categories";
+import PostsGrid from "./PostsGrid";
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+};
+
+export default function FilterablePost({ posts, categories }: Props) {
+  const [selected, setSelected] = useState("All Posts");
+  const filtered =
+    selected === "All Posts"
+      ? posts
+      : posts.filter((post) => post.category === selected);
+  return (
+    <section>
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={["All Posts", ...categories]}
+        selected={selected}
+        onClick={(selected) => setSelected(selected)}
+      />
+    </section>
+  );
+}
