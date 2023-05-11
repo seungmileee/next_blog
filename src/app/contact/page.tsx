f0ab5001466479a309e4e76dea267545b428dd66
seungmileee <dltmdal0928@naver.com>
2023-05-11 15:37:38 +0900

    feat: contact 페이지 입력폼 완성

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,36 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { SiVelog } from "react-icons/si";
+
+const LINKS = [
+  { icons: <AiFillGithub />, url: "https://github.com/seungmileee" },
+  {
+    icons: <AiFillLinkedin />,
+    url: "https://www.linkedin.com/in/%EC%8A%B9%EB%AF%B8-%EC%9D%B4-1533b8254/",
+  },
+  { icons: <SiVelog />, url: "https://velog.io/@dltmdal0928" },
+];
+
 export default function ContactPage() {
-  return <p>연락하기</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>dltmdal0928@gmail.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, idx) => (
+          <a
+            key={idx}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-sky-400"
+          >
+            {link.icons}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold m-8">Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
