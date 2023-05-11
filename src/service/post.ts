d34173a59b2c75f38667210a9f74523176be66d7
seungmileee <dltmdal0928@naver.com>
2023-05-11 12:34:01 +0900

    feat: 블로그 포스트 클릭하면 해당 포스트 내용 나오도록 완성

--- src/service/post.ts
+++ src/service/post.ts
@@ -12,6 +12,20 @@ export type Post = {
   featured: boolean;
 };
 
+export type PostData = Post & { content: string };
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const metadata = await getAllPosts().then((post) =>
+    post.find((post) => post.path === fileName)
+  );
+  if (!metadata)
+    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+  const content = await readFile(filePath, "utf-8");
+  return { ...metadata, content };
+}
+
 export async function getMayLikePosts(): Promise<Post[]> {
   return getAllPosts().then((post) => post.filter((post) => !post.featured));
 }
