b5db722b82f089b957d073dbeca4d77fa959e187
seungmileee <dltmdal0928@naver.com>
2023-05-11 14:22:30 +0900

    feat: 이전, 다음 포스트 보여주기 기능 로직 완성

--- src/service/post.ts
+++ src/service/post.ts
@@ -12,7 +12,11 @@ export type Post = {
   featured: boolean;
 };
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
@@ -20,8 +24,11 @@ export async function getPostData(fileName: string): Promise<PostData> {
   const post = posts.find((post) => post.path === fileName);
   if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
 
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length ? posts[index + 1] : null;
   const content = await readFile(filePath, "utf-8");
-  return { ...post, content };
+  return { ...post, content, next, prev };
 }
 
 export async function getMayLikePosts(): Promise<Post[]> {
